29350560966b96dd8dcfd504ba3d6f81ba0ac21a
Jonathan <63370021+FineFindus@users.noreply.github.com>
2022-03-28 21:34:27 +0200

    Updated Test

--- Formula/artem.rb
+++ Formula/artem.rb
@@ -24,21 +24,6 @@ class Artem < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test artem`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    img = test_fixtures("test.png")
-    output = shell_output("#{bin}/artem #{img} --no-color")
-    assert_match "
-
-
-
-      ", output
+    assert_match "artem 0.6.1", shell_output("#{bin}/artem -V")
   end
 end
