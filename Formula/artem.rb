3748724a049eaa6a4fafce50daba48b3b1ef915f
Jonathan <63370021+FineFindus@users.noreply.github.com>
2022-06-01 16:00:45 +0000

    artem 1.1.4

--- Formula/artem.rb
+++ Formula/artem.rb
@@ -5,8 +5,8 @@
 class Artem < Formula
   desc "Is a small cli program written in rust to easily convert images to ascii art"
   homepage "https://github.com/FineFindus/artem"
-  url "https://github.com/FineFindus/artem/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "5c9de1b5c4695b5a8dc5ed3e6ad3f9010136960b51516a0007c06201e5def8de"
+  url "https://github.com/FineFindus/artem/archive/refs/tags/v1.1.4.tar.gz"
+  sha256 "3f29e64d82c9b96c7be8393b260300d90ebcf919b17d97238789259bce8b546a"
   license "MPL-2.0"
 
   bottle do
